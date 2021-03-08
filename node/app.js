4c6e62d107e2d6fcb6a4fdcce837f6255b962b2c
elkasmiadnane <73801424+elkasmiadnane@users.noreply.github.com>
2021-03-08 17:01:48 +0100

    update

--- node/app.js
+++ node/app.js
@@ -1,17 +1,28 @@
-
-
 const express = require('express');
+
 const app = express();
+
+//const contactme = document.querySelector('.contactme.html');
 
 const port = process.env.port || 3000;
 
-let name = document.getElementById('name');
+//let name = contactme.getElementById('name');
 
 //port = 
 
+app.use(express.json());
+
 app.get('/' , (req,res)=>{
-    req.send('yes');
-})
+    res.send('yes');
+});
+
+app.get('/n' , (req,res)=>{
+    
+
+    
+
+    res.send(n);
+});
 
 
 app.listen(port , ()=> console.log(`Listening on ${port}`));
